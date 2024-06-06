837d1132455c26c889f25164979ad921171efa91
stormer3428 <azertyuiopcharles@gmail.com>
2024-06-06 13:24:51 +0200

    Added unit testing

---
+++ tests/unit.test.js
@@ -0,0 +1,16 @@
+const { app, initializeDbConnection } = require('../search.js');
+const request = require('supertest');
+const {calculateExpression} = require("../public/script.js")
+
+describe('Unit testing', () => {
+    it('test expression evaluation', async () => {
+        expect(calculateExpression("2")).toBe(2);
+    });
+    
+    it('test operation priority', async () => {
+        expect(calculateExpression("1+2*3+4")).toBe(11);
+        expect(calculateExpression("(1+2)*3+4")).toBe(13);
+        expect(calculateExpression("1+2*(3+4)")).toBe(15);
+        expect(calculateExpression("(1+2)*(3+4)")).toBe(21);
+    });
+});
